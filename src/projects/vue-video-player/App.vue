85dd1bdeb6512b681ed39583f5bccccf75109b7f
Surmon <surmon@qq.com>
2018-09-19 00:09:15 +0800

    update dependencies

--- src/projects/vue-video-player/App.vue
+++ src/projects/vue-video-player/App.vue
@@ -1,6 +1,6 @@
 <template>
   <app-main name="Vue-Video-Player"
-            desc="Video Player for Vue"
+            desc="Video.js component for Vue"
             link="https://github.com/surmon-china/vue-video-player">
     <div slot="actions">
       <md-button href="https://github.com/surmon-china/vue-video-player" 
@@ -18,22 +18,22 @@
       <md-button>HI! Star~</md-button>
     </div>
     <div slot="content">
-      <!-- <example></example> -->
-      <test></test>
+      <example></example>
+      <!-- <test></test> -->
     </div>
   </app-main>
 </template>
 
 <script>
   import AppMain from '@/main.vue'
-  // import Example from './Example.vue'
-  import Test from './Test.vue'
+  import Example from './Example.vue'
+  // import Test from './Test.vue'
   export default {
     name: 'app',
     components: {
       AppMain,
-      // Example
-      Test
+      Example,
+      // Test
     }
   }
 </script>
