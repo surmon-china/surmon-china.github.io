e1db7d9eaaf6fd4c736b300b710fe9d051168542
surmon-china <surmon@foxmail.com>
2019-10-18 00:26:34 +0800

    Upgrade

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
+
 import Vue from 'vue'
+import Element from 'element-ui'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import 'normalize.css/normalize.css'
+import 'element-ui/lib/theme-chalk/index.css'
+import '@/assets/styles/app.scss'
+
 Vue.config.productionTip = false
+Vue.use(Element)
 
 new Vue({
   router,
--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Home from './components/Home.vue'
+import Example from './components/Example.vue'
+import RouteView from './components/RouteView.vue'
 
 Vue.use(Router)
 
@@ -14,12 +16,25 @@ export default new Router({
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/example',
+      name: 'example',
+      component: Example
+    },
+    {
+      path: '/examples',
+      name: 'examples',
+      component: RouteView,
+      children: [
+        {
+          path: 'vue-awesome-swiper',
+          name: 'vue-awesome-swiper',
+          component: () => import('./projects/vue-awesome-swiper'),
+        }
+      ]
+    },
+    {
+      path: '*',
+      redirect: '/'
     }
   ]
 })
