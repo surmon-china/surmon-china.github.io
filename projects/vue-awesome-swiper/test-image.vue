75d13c4986cae196ad1538ada713f5eac3e06e2b
surmon-china <surmon@foxmail.com>
2020-03-09 16:57:23 +0800

    Update

---
+++ projects/vue-awesome-swiper/test-image.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="swiper-lazy-box">
+    <img class="swiper-lazy" :data-src="src" ref="imgEle">
+  </div>
+</template>
+
+<script>
+  import { swiper, swiperSlide } from 'vue-awesome-swiper'
+  import 'swiper/dist/css/swiper.css'
+
+  export default {
+    name: 'swiper-example-03',
+    components: {
+      swiper,
+      swiperSlide
+    },
+    exampleData: {
+      name: 'Pagination'
+    },
+    name: 'lazyImage',
+    props: {
+      src: String
+    },
+    updated() {
+      this.$refs.imgEle.src = this.src
+    }
+  }
+</script>
---
+++ projects/vue-quill-editor/test-size.vue
@@ -0,0 +1,94 @@
+<template>
+  <md-card>
+    <md-card-actions>
+      <div class="md-subhead">
+        <span>02 Example (theme bubble)</span>
+      </div>
+      <md-button class="md-icon-button" target="_blank" href="https://github.com/surmon-china/vue-quill-editor/tree/master/examples/02-example.vue">
+        <md-icon>code</md-icon>
+      </md-button>
+    </md-card-actions>
+    <md-card-media>
+      <div class="quill-editor-example">
+        <!-- quill-editor -->
+        <quill-editor
+          class="editor-example"
+          ref="myTextEditor"
+          :content="content"
+          :options="editorOption"
+          @blur="onEditorBlur($event)"
+          @focus="onEditorFocus($event)"
+          @change="onEditorChange($event)">
+        </quill-editor>
+        <div class="html ql-editor" v-html="content"></div>
+      </div>
+    </md-card-media>
+  </md-card>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        name: 'base-example',
+        content: '<h2>I am Example 2</h2>',
+        editorOption: {
+          theme: 'bubble',
+          placeholder: "输入任何内容，支持html",
+          modules: {
+            toolbar: [
+              ['bold', 'italic', 'underline', 'strike'],
+              [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+              [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+              [{ 'color': [] }, { 'background': [] }],
+              [{ 'font': [] }],
+              [{ 'align': [] }],
+              ['link', 'image'],
+              ['clean']
+            ]
+          }
+        }
+      }
+    },
+    methods: {
+      onEditorBlur(editor) {
+        console.log('editor blur!', editor)
+      },
+      onEditorFocus(editor) {
+        console.log('editor focus!', editor)
+      },
+      onEditorChange({ editor, html, text }) {
+        // console.log('editor change!', editor, html, text)
+        this.content = html
+      }
+    },
+    computed: {
+      editor() {
+        return this.$refs.myTextEditor.quill
+      }
+    },
+    mounted() {
+      // console.log('this is my editor', this.editor)
+    }
+  }
+</script>
+
+<style lang="scss">
+  .editor-example {
+    border: 1px solid #ccc;
+  }
+
+  .ql-container .ql-editor {
+    min-height: 20em;
+    padding-bottom: 1em;
+    max-height: 25em;
+  }
+
+  .html {
+    height: 9em;
+    overflow-y: auto;
+    border: 1px solid #ccc;
+    border-top: none;
+    resize: vertical;
+  }
+</style>
