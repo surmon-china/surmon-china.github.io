90131caa1a18a9c76373f74d852b3c58dbc4dc86
surmon-china <surmon.me@gmail.com>
2022-03-14 07:39:54 +0800

    chore: NIP

--- src/plugins/codemirror.ts
+++
@@ -1,6 +0,0 @@
-import { defineNuxtPlugin } from '#app'
-import VueCodemirror from '@/npms/vue-codemirror/src'
-
-export default defineNuxtPlugin((nuxtApp) => {
-  // nuxtApp.vueApp.use(VueCodemirror)
-})
