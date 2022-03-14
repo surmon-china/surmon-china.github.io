8d93475a8a5186fb063dbdb92bf1c7ac0310d6c9
surmon-china <surmon.me@gmail.com>
2022-03-14 20:58:15 +0800

    chore: v1 vercel script

---
+++ v1-vercel.sh
@@ -0,0 +1,11 @@
+# https://www.konabos.com/blog/branch-and-conditional-deploys-in-vercel
+# https://vercel.com/support/articles/how-do-i-use-the-ignored-build-step-field-on-vercel
+if [[ "$VERCEL_GIT_COMMIT_REF" == "$BRANCH_TO_BUILD" ]] ; then
+  # Proceed with the build
+  echo "✅ - Build can proceed"
+  exit 1;
+else
+  # Don't build
+  echo "🛑 - Build canceled"
+  exit 0;
+fi
