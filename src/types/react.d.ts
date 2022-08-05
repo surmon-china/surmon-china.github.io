c0348503a97f15b87fb3eae9e58d8efaebce7140
surmon-china <surmon.me@gmail.com>
2022-08-06 02:42:30 +0800

    feat: React type hack

---
+++ src/types/react.d.ts
@@ -0,0 +1,7 @@
+// MARK: Unable to use React types
+// https://github.com/vuejs/core/issues/1033
+// https://github.com/johnsoncodehk/volar/discussions/592
+// https://github.com/storybookjs/storybook/issues/12505
+
+declare module 'react'
+declare module 'react-dom/client'
