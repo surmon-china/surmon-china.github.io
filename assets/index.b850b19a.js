320dbf8fdbdb7fc7df4bea3a6cf914991df718c2
surmon-china <surmon-china@users.noreply.github.com>
2022-08-05 21:18:31 +0000

    deploy: 3f170c317f11de2676de295ead05a2068525c233

---
+++ assets/index.b850b19a.js
@@ -0,0 +1,8 @@
+import{s as L,M as e}from"./index.c29885a7.js";import"./index.4ea68786.js";import"./index.8e230086.js";import"./index.9b0e96e7.js";const E=`CREATE TABLE shop (
+    article INT(4) UNSIGNED ZEROFILL DEFAULT '0000' NOT NULL,
+    dealer  CHAR(20)                 DEFAULT ''     NOT NULL,
+    price   DOUBLE(16,2)             DEFAULT '0.00' NOT NULL,
+    PRIMARY KEY(article, dealer));
+INSERT INTO shop VALUES
+    (1,'A',3.45),(1,'B',3.99),(2,'A',10.99),(3,'B',1.45),
+    (3,'C',1.69),(3,'D',1.25),(4,'D',19.95);`,t={language:()=>L({dialect:e}),code:E};export{t as default};
--- assets/index.c8a91a75.js
+++
@@ -1,8 +0,0 @@
-import{s as e,P as n}from"./index.66c4139b.js";import"./index.deea36a6.js";import"./index.76ee433a.js";import"./index.3732e9dd.js";const E=`BEGIN
-    SELECT * INTO STRICT myrec FROM emp WHERE empname = myname;
-    EXCEPTION
-        WHEN NO_DATA_FOUND THEN
-            RAISE EXCEPTION 'employee % not found', myname;
-        WHEN TOO_MANY_ROWS THEN
-            RAISE EXCEPTION 'employee % not unique', myname;
-END;`,a={language:()=>e({dialect:n}),code:E};export{a as default};
