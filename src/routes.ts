cb8443e14119f21b7ad1ce0c5866d64e53e51183
surmon-china <surmon.me@gmail.com>
2022-08-06 02:43:05 +0800

    feat: projects config to routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,50 +1,24 @@
 import type { RouteRecordRaw } from 'vue-router'
-import { Repository } from './config'
+import { PROJECTS } from './config'
 
-export const routes: RouteRecordRaw[] = [
-  {
-    name: 'home',
-    path: '/',
-    component: () => import('@/pages/home.vue')
-  },
-  {
-    name: Repository.Readme,
-    path: `/readme-md`,
-    component: () => import('@/pages/readme-md.vue')
-  },
-  {
-    name: Repository.NaiveBayes,
-    path: `/naivebayes`,
-    component: () => import('@/pages/naivebayes.vue')
-  },
-  {
-    name: Repository.VueAwesomeSwiper,
-    path: '/' + Repository.VueAwesomeSwiper,
-    component: () => import('@/pages/vue-awesome-swiper.vue')
-  },
-  {
-    name: Repository.VueCodemirror,
-    path: '/' + Repository.VueCodemirror,
-    component: () => import('@/pages/vue-codemirror.vue')
-  },
-  {
-    name: Repository.VueDragZone,
-    path: '/' + Repository.VueDragZone,
-    component: () => import('@/pages/vue-drag-zone.vue')
-  },
-  {
-    name: Repository.VueQuillEditor,
-    path: '/' + Repository.VueQuillEditor,
-    component: () => import('@/pages/vue-quill-editor.vue')
-  },
-  {
-    name: Repository.VueTouchRipple,
-    path: '/' + Repository.VueTouchRipple,
-    component: () => import('@/pages/vue-touch-ripple.vue')
-  },
-  {
-    name: 'error',
-    path: '/:error(.*)',
-    component: () => import('@/pages/home.vue')
-  }
-]
+const pageImporters = import.meta.glob<{ default: any }>('@/pages/*.vue')
+
+const homeRoute: RouteRecordRaw = {
+  name: 'home',
+  path: '/',
+  component: pageImporters['/src/pages/home.vue']
+}
+
+const errorRoute: RouteRecordRaw = {
+  name: 'error',
+  path: '/:error(.*)',
+  component: pageImporters['/src/pages/home.vue']
+}
+
+const projectRoutes = Object.values(PROJECTS).map<RouteRecordRaw>((project) => ({
+  name: project.route,
+  path: '/' + project.route,
+  component: pageImporters[`/src/pages/${project.route}.vue`]
+}))
+
+export const routes: RouteRecordRaw[] = [homeRoute, ...projectRoutes, errorRoute]
