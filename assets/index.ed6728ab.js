9fd5f2fcd2105c297b22190f47de193545e1f38e
surmon-china <surmon-china@users.noreply.github.com>
2022-08-06 15:59:07 +0000

    deploy: 8a80c77170ca2b242bdfb3d55f6d1b4a8e3dad08

---
+++ assets/index.ed6728ab.js
@@ -0,0 +1,28 @@
+import{h as n}from"./index.a789bc82.js";import"./index.8d782e30.js";import"./index.9d33a860.js";import"./index.b7330ebb.js";import"./index.36787cbf.js";import"./index.cb9b9684.js";const t=`<html style="color: green">
+  <!-- this is a comment -->
+  <head>
+    <title>Mixed HTML Example</title>
+    <style>
+      h1 {
+        font-family: comic sans;
+        color: #f0f;
+      }
+      div {
+        background: yellow !important;
+      }
+      body {
+        max-width: 50em;
+        margin: 1em 2em 1em 5em;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>Mixed HTML Example</h1>
+    <script>
+      function jsFunc(arg1, arg2) {
+        if (arg1 && arg2) document.body.innerHTML = 'achoo'
+      }
+    <\/script>
+  </body>
+</html>
+`,l={language:n,code:t};export{l as default};
