8274e2810b02aeb7ee72144eac9686899a4509eb
surmon-china <surmon.me@gmail.com>
2022-08-07 06:23:04 +0800

    feat(videojs-player): improve Flv example

--- examples/videojs-player/06-flv.vue
+++ examples/videojs-player/06-flv.vue
@@ -2,25 +2,22 @@
   <video-player
     class="video-player vjs-theme-forest"
     poster="/images/example/3.jpg"
-    src="/videos/sample-video.flv"
     crossorigin="anonymous"
     playsinline
     controls
     :volume="0.6"
     :height="320"
     :techOrder="['html5', 'flvjs']"
+    @mounted="handleMounted"
   />
 </template>
 
 <script lang="ts">
-  import videojs from 'video.js'
+  import videojs, { VideoJsPlayer } from 'video.js'
   import { defineComponent } from 'vue'
   import { VideoPlayer } from '@videojs-player/vue'
-  import { FlvJsTech } from './06-flv-video-tech'
   import 'video.js/dist/video-js.css'
   import '@videojs/themes/dist/forest/index.css'
-
-  videojs.registerTech('Flvjs', FlvJsTech)
 
   export default defineComponent({
     name: 'vue-flv-player-example',
@@ -28,6 +25,17 @@
     url: import.meta.url,
     components: {
       VideoPlayer
+    },
+    setup() {
+      const handleMounted = ({ player }: { player: VideoJsPlayer }) => {
+        // flv.js runs only in the browser environment and does an asynchronous processing for SSR prerender.
+        import('./06-flv-video-tech').then(({ FlvJsTech }) => {
+          videojs.registerTech('Flvjs', FlvJsTech)
+          player.src('/videos/sample-video.flv')
+        })
+      }
+
+      return { handleMounted }
     }
   })
 </script>
--- examples/videojs-player/index.ts
+++ examples/videojs-player/index.ts
@@ -1,58 +1,27 @@
-import { defineComponent, defineAsyncComponent } from 'vue'
 import { ExampleComponentConfig } from '@/transforms/example'
 
-// For SSR prerender
-import vueFlvComponentRaw from './06-flv.vue?raw'
-const vueFlvComponent = defineAsyncComponent(() => {
-  return import.meta.env.SSR ? Promise.resolve(defineComponent({})) : import('./06-flv.vue')
-})
-vueFlvComponent.name = 'vue-flv-player-example'
-vueFlvComponent.title = 'FLV player (Vue)'
-const vueFlvExample: ExampleComponentConfig = {
-  component: vueFlvComponent,
-  raw: vueFlvComponentRaw,
+import AdvancedPlayer from './01-advanced-player/index.vue'
+import AdvancedPlayerRaw from './01-advanced-player/index.vue?raw'
+const vueAdvanceExample: ExampleComponentConfig = {
+  component: AdvancedPlayer,
+  raw: AdvancedPlayerRaw,
   language: 'vue'
 }
 
-import reactBasicPlayer from './07-react'
-import reactBasicPlayerRaw from './07-react?raw'
-const reactBasicExample: ExampleComponentConfig = {
-  component: reactBasicPlayer,
-  raw: reactBasicPlayerRaw,
+import ReactBasicPlayer from './07-react'
+import ReactBasicPlayerRaw from './07-react?raw'
+const reactBasicExample = {
+  component: ReactBasicPlayer,
+  raw: ReactBasicPlayerRaw,
   language: 'typescript'
 }
 
-const vueComponents = import.meta.glob<{ default: any }>(
-  [
-    './01-advanced-player/index.vue',
-    './02-basic.vue',
-    './03-custom.vue',
-    './04-hls.vue',
-    './05-dash.vue'
-  ],
-  {
-    eager: true
-  }
-)
-
-const vueComponentRaws = import.meta.glob<string>(
-  [
-    './01-advanced-player/index.vue',
-    './02-basic.vue',
-    './03-custom.vue',
-    './04-hls.vue',
-    './05-dash.vue'
-  ],
-  {
-    as: 'raw',
-    eager: true
-  }
-)
-
+const vueComponents = import.meta.glob<{ default: any }>('./*.vue', { eager: true })
+const vueComponentRaws = import.meta.glob<string>('./*.vue', { as: 'raw', eager: true })
 const vueExamples: Array<ExampleComponentConfig> = Object.keys(vueComponents).map((id) => ({
   component: vueComponents[id].default,
   raw: vueComponentRaws[id],
   language: 'vue'
 }))
 
-export default [...vueExamples, vueFlvExample, reactBasicExample]
+export default [vueAdvanceExample, ...vueExamples, reactBasicExample]
