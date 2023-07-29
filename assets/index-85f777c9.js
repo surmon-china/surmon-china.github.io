a7e429b9327b6821e6188a3cf7a73f4065fd8906
surmon-china <surmon-china@users.noreply.github.com>
2023-07-29 23:49:31 +0000

    deploy: 76f6bbb44c317df145f9406b8788c1c49c9365d8

---
+++ assets/index-85f777c9.js
@@ -0,0 +1,28 @@
+import{h as n}from"./index-d37427ff.js";import"./index-b3ae71e7.js";import"./index-f05a85ab.js";import"./index-729aeab9.js";import"./index-b2ec1b46.js";import"./index-d2e08b08.js";const t=`<html style="color: green">
+  <!-- this is a comment -->
+  <head>
+    <title>Mixed HTML Example</title>
+    <style>
+      h1 {
+        font-family: comic sans;
+        color: #f0f;
+      }
+      div {
+        background: yellow !important;
+      }
+      body {
+        max-width: 50em;
+        margin: 1em 2em 1em 5em;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>Mixed HTML Example</h1>
+    <script>
+      function jsFunc(arg1, arg2) {
+        if (arg1 && arg2) document.body.innerHTML = 'achoo'
+      }
+    <\/script>
+  </body>
+</html>
+`,l={language:n,code:t};export{l as default};
---
+++ assets/index-bff96614.js
@@ -0,0 +1,35 @@
+import{c as n}from"./index-b2ec1b46.js";import"./index-b3ae71e7.js";import"./index-f05a85ab.js";import"./index-729aeab9.js";const o=`/* Some example CSS */
+
+@import url('something.css');
+
+body {
+  margin: 0;
+  padding: 3em 6em;
+  font-family: tahoma, arial, sans-serif;
+  color: #000;
+}
+
+#navigation a {
+  font-weight: bold;
+  text-decoration: none !important;
+}
+
+h1 {
+  font-size: 2.5em;
+}
+
+h2 {
+  font-size: 1.7em;
+}
+
+h1:before,
+h2:before {
+  content: '::';
+}
+
+code {
+  font-family: courier, monospace;
+  font-size: 80%;
+  color: #418a8a;
+}
+`,m={language:n,code:o};export{m as default};
