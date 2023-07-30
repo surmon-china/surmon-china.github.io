1ec63f4015e7fb6d197fff8c8ef8b8fd4709fe8a
Surmon <surmon.me@gmail.com>
2023-07-30 07:10:17 +0700

    chore: ad

--- src/components/mammon/adsense-responsive.vue
+++ src/components/mammon/adsense-responsive.vue
@@ -1,19 +1,13 @@
 <script lang="ts" setup>
   import { GOOGLE_ADSENSE_CLIENT_ID } from '@/config'
-  const props = defineProps({
-    rootClass: {
-      type: String,
-      default: ''
-    }
-  })
+  const props = defineProps<{ rootClass: string }>()
 </script>
 
 <template>
   <Adsense
     :class="`g-a-mammon ${rootClass}`"
+    ins-style="display:inline-block;width:960px;height:200px"
     :data-ad-client="GOOGLE_ADSENSE_CLIENT_ID"
-    :data-full-width-responsive="true"
-    data-ad-format="auto"
     data-ad-slot="5885388786"
   ></Adsense>
 </template>
@@ -23,6 +17,6 @@
   @import '@/styles/mixins.scss';
   .g-a-mammon {
     width: 100%;
-    height: 10rem;
+    height: 200px;
   }
 </style>
