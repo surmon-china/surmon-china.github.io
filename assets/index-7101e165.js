459023c82fe887401002ccce1a4c7bd7d7fe525d
surmon-china <surmon-china@users.noreply.github.com>
2023-08-28 05:12:13 +0000

    deploy: 48fddb7987c0377bbe6a5c93f33160cf7f6ffdb3

---
+++ assets/index-7101e165.js
@@ -0,0 +1,35 @@
+import{c as n}from"./index-21077091.js";import"./index-677ac83b.js";import"./index-d13c71b4.js";import"./index-da348f64.js";const o=`/* Some example CSS */
+
+@import url('something.css');
+
+body {
+  margin: 0;
+  padding: 3em 6em;
+  font-family: tahoma, arial, sans-serif;
+  color: #000;
+}
+
+#navigation a {
+  font-weight: bold;
+  text-decoration: none !important;
+}
+
+h1 {
+  font-size: 2.5em;
+}
+
+h2 {
+  font-size: 1.7em;
+}
+
+h1:before,
+h2:before {
+  content: '::';
+}
+
+code {
+  font-family: courier, monospace;
+  font-size: 80%;
+  color: #418a8a;
+}
+`,m={language:n,code:o};export{m as default};
