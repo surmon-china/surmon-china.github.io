ae6dadc22bf29b91ea46c3e15626241581a4d831
surmon-china <surmon-china@users.noreply.github.com>
2023-08-31 22:24:40 +0000

    deploy: 07b9fd9565932aaf589227547b4d8b82f15a312d

--- assets/index-e0eb65b7.js
+++
@@ -1,8 +0,0 @@
-import{s as e,P as n}from"./index-0f4fe9d3.js";import"./index-0413a79e.js";import"./index-e9ad3e5c.js";import"./index-63b7b75f.js";const E=`BEGIN
-    SELECT * INTO STRICT myrec FROM emp WHERE empname = myname;
-    EXCEPTION
-        WHEN NO_DATA_FOUND THEN
-            RAISE EXCEPTION 'employee % not found', myname;
-        WHEN TOO_MANY_ROWS THEN
-            RAISE EXCEPTION 'employee % not unique', myname;
-END;`,a={language:()=>e({dialect:n}),code:E};export{a as default};
