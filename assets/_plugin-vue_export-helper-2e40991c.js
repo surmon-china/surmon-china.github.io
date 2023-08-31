745cf6466d969ac417528782fb8c6df621a6b260
surmon-china <surmon-china@users.noreply.github.com>
2023-08-31 23:05:59 +0000

    deploy: 2054f333f0868f64d7495883d0fbec6994278514

---
+++ assets/_plugin-vue_export-helper-2e40991c.js
@@ -0,0 +1,1 @@
+import{f as c,ae as f,af as u,ag as p,ah as l}from"./index-544c9a3d.js";function v(t,a){const e=c({});return f(()=>{const s=p(t),{title:o,titleTemplate:n,...r}=s;e.value={title:o,titleTemplate:n,meta:l(r)}}),u(e,a)}const _=(t,a)=>{const e=t.__vccOpts||t;for(const[s,o]of a)e[s]=o;return e};export{_,v as u};
---
+++ assets/index-ce939dac.js
@@ -0,0 +1,28 @@
+import{h as n}from"./index-ea4bdef1.js";import"./index-ca6a7ec7.js";import"./index-66c151a7.js";import"./index-544c9a3d.js";import"./index-238cdab0.js";import"./index-544d902a.js";const t=`<html style="color: green">
+  <!-- this is a comment -->
+  <head>
+    <title>Mixed HTML Example</title>
+    <style>
+      h1 {
+        font-family: comic sans;
+        color: #f0f;
+      }
+      div {
+        background: yellow !important;
+      }
+      body {
+        max-width: 50em;
+        margin: 1em 2em 1em 5em;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>Mixed HTML Example</h1>
+    <script>
+      function jsFunc(arg1, arg2) {
+        if (arg1 && arg2) document.body.innerHTML = 'achoo'
+      }
+    <\/script>
+  </body>
+</html>
+`,l={language:n,code:t};export{l as default};
