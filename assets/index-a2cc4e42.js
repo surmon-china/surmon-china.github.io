265eae6634a8b2704510bced3dfa7d6fc8291019
surmon-china <surmon-china@users.noreply.github.com>
2023-09-01 16:48:21 +0000

    deploy: d2a9be68697a8c48c4ee222fecf0f3fb32e81666

---
+++ assets/index-a2cc4e42.js
@@ -0,0 +1,28 @@
+import{h as n}from"./index-2684fc65.js";import"./index-956722b6.js";import"./index-77c59bb2.js";import"./index-8c0767fa.js";import"./index-5da97100.js";import"./index-8e2d5c89.js";const t=`<html style="color: green">
+  <!-- this is a comment -->
+  <head>
+    <title>Mixed HTML Example</title>
+    <style>
+      h1 {
+        font-family: comic sans;
+        color: #f0f;
+      }
+      div {
+        background: yellow !important;
+      }
+      body {
+        max-width: 50em;
+        margin: 1em 2em 1em 5em;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>Mixed HTML Example</h1>
+    <script>
+      function jsFunc(arg1, arg2) {
+        if (arg1 && arg2) document.body.innerHTML = 'achoo'
+      }
+    <\/script>
+  </body>
+</html>
+`,l={language:n,code:t};export{l as default};
--- assets/index-dbaa2277.js
+++
@@ -1,8 +0,0 @@
-import{s as e,P as n}from"./index-06347cd7.js";import"./index-580114bb.js";import"./index-4413b224.js";import"./index-f3e20788.js";const E=`BEGIN
-    SELECT * INTO STRICT myrec FROM emp WHERE empname = myname;
-    EXCEPTION
-        WHEN NO_DATA_FOUND THEN
-            RAISE EXCEPTION 'employee % not found', myname;
-        WHEN TOO_MANY_ROWS THEN
-            RAISE EXCEPTION 'employee % not unique', myname;
-END;`,a={language:()=>e({dialect:n}),code:E};export{a as default};
