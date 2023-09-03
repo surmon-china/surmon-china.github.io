41da0dadf96f784e2846173f3f68a67238ed85f4
surmon-china <surmon-china@users.noreply.github.com>
2023-09-03 21:41:33 +0000

    deploy: 5d918dd6b8570c0c26ad2d2c8bf9c53b388ba4b2

---
+++ assets/index-6da1ab21.js
@@ -0,0 +1,28 @@
+import{h as n}from"./index-95b7a98a.js";import"./index-77812cc6.js";import"./index-e77fc07e.js";import"./index-ada4f49e.js";import"./index-765e6c7f.js";import"./index-dba0e3b4.js";const t=`<html style="color: green">
+  <!-- this is a comment -->
+  <head>
+    <title>Mixed HTML Example</title>
+    <style>
+      h1 {
+        font-family: comic sans;
+        color: #f0f;
+      }
+      div {
+        background: yellow !important;
+      }
+      body {
+        max-width: 50em;
+        margin: 1em 2em 1em 5em;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>Mixed HTML Example</h1>
+    <script>
+      function jsFunc(arg1, arg2) {
+        if (arg1 && arg2) document.body.innerHTML = 'achoo'
+      }
+    <\/script>
+  </body>
+</html>
+`,l={language:n,code:t};export{l as default};
---
+++ assets/index-905a668d.js
@@ -0,0 +1,8 @@
+import{s as L,M as e}from"./index-d4381647.js";import"./index-e77fc07e.js";import"./index-ada4f49e.js";import"./index-77812cc6.js";const E=`CREATE TABLE shop (
+    article INT(4) UNSIGNED ZEROFILL DEFAULT '0000' NOT NULL,
+    dealer  CHAR(20)                 DEFAULT ''     NOT NULL,
+    price   DOUBLE(16,2)             DEFAULT '0.00' NOT NULL,
+    PRIMARY KEY(article, dealer));
+INSERT INTO shop VALUES
+    (1,'A',3.45),(1,'B',3.99),(2,'A',10.99),(3,'B',1.45),
+    (3,'C',1.69),(3,'D',1.25),(4,'D',19.95);`,t={language:()=>L({dialect:e}),code:E};export{t as default};
